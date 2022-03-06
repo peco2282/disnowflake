cd8cb347812930dff2782c12f03cbd30a36d70f7
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-03-07 08:12:31 +0900

    add description

--- disnowflake/__init__.py
+++ disnowflake/__init__.py
@@ -1,1 +1,17 @@
+"""
+Discord Snowflake changer
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Snowflake change to time
+
+:copyright: (c) 2022-present peco2282
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'disnowflake'
+__author__ = 'peco2282'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present Rapptz'
+__version__ = '1.0.0'
+
 from .disnowflake import snow_date
